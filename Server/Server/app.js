4b43419f398d549b48844a8c6d78cc535e80431c
Medgyesi Zsolt <zmedgyes@gmail.com>
2017-11-20 11:15:17 +0100

    parancs küldés/fogadás, érték konverziók

--- Server/Server/app.js
+++ Server/Server/app.js
@@ -4,6 +4,9 @@ const Readline = SerialPort.parsers.Readline;
 
 var port;
 var serialport;
+var cmdBuffer = ["send 060001"]
+
+
 var lookupPort = (callback) => {
     SerialPort.list(function (err, ports) {
         ports.forEach(function (portItem) {
@@ -35,13 +38,34 @@ var openSerial = (callback) => {
         callback
     )
 }
+
+var processCommand = (code) => {
+    if (code.includes("getbufferlength")) {
+        serialport.write(cmdBuffer.length.toString())
+    }
+    else if (code.includes("getnext")) {
+        serialport.write(cmdBuffer[0])
+    }
+    else if (code.includes("radio_rx")) {
+        var hex = code.split(" ")[2]
+        var buf = new Buffer(hex, "hex")
+        console.log("to: " + buf.readInt8(0))
+        console.log("from: " + buf.readInt8(1))
+        var type = buf.readInt8(2)
+        console.log("type: " + type)
+        if (type == 2) {
+            console.log("Amps: "+buf.readFloatBE(3))
+        }
+
+    }
+}
+
 var addLineParser = (callback) => {
     const parser = serialport.pipe(new Readline({ delimiter: '\r\n' }));
     parser.on('data', (chunk) => {
         console.log(chunk)
-        if (chunk.includes("check")) {
-            serialport.write("mycmd")
-        }
+        processCommand(chunk)
+        
     });
     async.setImmediate(callback)
 }
